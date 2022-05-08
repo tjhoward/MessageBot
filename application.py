e9565f47fe1938608b213a487b55f1b6aded9862
Travis Howard <tjhowar4@asu.edu>
2022-05-07 23:27:21 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
 from flask import Flask 
 from threading import Thread
+import os #to get environment variable from AWS
+
+api_key = os.environ['API_KEY']
 
 application = Flask('__name__')
 
 @application.route('/')
 def hellow_world():
-    return "Hello. I am alive!"
+    return f"Hello. I am alive! {api_key}"
 
 def run():
   application.run(host='0.0.0.0',port=8080)
