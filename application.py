f105d5218fb2c574ab0e6b645910ddb6472420a4
Travis Howard <61389527+tjhoward@users.noreply.github.com>
2022-05-07 21:26:25 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#from threading import Thread
+from threading import Thread
 
 application = Flask('__name__')
 
@@ -7,9 +7,9 @@ application = Flask('__name__')
 def hellow_world():
     return "Hello. I am alive!"
 
-#def run():
-#  app.run(host='0.0.0.0',port=8080)
+def run():
+  app.run(host='0.0.0.0',port=8080)
 
-#def keep_alive():
-#    t = Thread(target=run)
-#    t.start()
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
