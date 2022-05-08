a2e93e1f34f5861eec38a7a8fa706f8b2d564c76
Travis Howard <tjhowar4@asu.edu>
2022-05-08 02:33:41 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,17 +1,19 @@
 from flask import Flask 
 from threading import Thread
 from main import start
+from nextcord.ext import commands##
 import os #to get environment variable from AWS
 
 api_key = os.environ['API_KEY']
-
+bot = commands.Bot(command_prefix = '.') ##
 application = Flask('__name__')
 
 @application.route('/')
 def hellow_world():
 
     #keep_alive()
-    start()
+    #start()
+    bot.run(token= api_key)
     return f"Hello. I am alive! {api_key}"
 
 def run():
