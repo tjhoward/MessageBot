5a3747d4d5aca3d5c67aff4cd47a999deeae9eb2
Travis Howard <61389527+tjhoward@users.noreply.github.com>
2022-05-07 19:47:43 -0700

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from email.mime import application
+from flask import Flask
+from threading import Thread
+
+application = Flask('__name__')
+
+@application.route('/')
+def hellow_world():
+    return "Hello. I am alive!"
+
+#def run():
+#  app.run(host='0.0.0.0',port=8080)
+
+#def keep_alive():
+#    t = Thread(target=run)
+#    t.start()
