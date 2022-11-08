58ec61666c8a496779a365bc4f1a420ac0809c4c
Travis Howard <tjhowar4@asu.edu>
2022-11-07 21:55:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -126,10 +126,9 @@ async def my_message(message):
         emoji = '\N{THUMBS UP SIGN}'
         await message.add_reaction(emoji)
 
-    elif channel.name == "musicc": #and str(message.author) == "resol":
+    elif channel.name == "musicc" and message.author != bot.user : #and str(message.author) == "resol":
         emoji = '\N{THUMBS UP SIGN}'
         x = str(message.author)
-        #await message.add_reaction(emoji)
         await channel.send(x)
         await channel.send(emoji)
 
